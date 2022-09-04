878e57ed87f366abd8bf948e0cfa86393115c862
shoaws <108819031+shoaws@users.noreply.github.com>
2022-09-04 11:18:29 +0000

    [Addいいね・コメント機能]

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about"
   root to: "homes#top"
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
