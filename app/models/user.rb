64bedee3112adcd1f3b264f8c54dcf26f07afd84
shoaws <108819031+shoaws@users.noreply.github.com>
2022-09-04 08:20:39 +0000

    [Addいいね機能]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -14,4 +15,6 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   get "home/top"=>"homes#top"
   get "home/about"=>"homes#about"
   root to: "homes#top"
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:index,:show,:edit,:update]
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
