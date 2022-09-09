9e5a97bbf7dd8b8bdba42c38d87a08e1fb295652
shoaws <108819031+shoaws@users.noreply.github.com>
2022-09-09 03:59:56 +0000

    [Add非同期ファイルの簡略化]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    #edirect_to request.referer
+    render :change_btn
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    #redirect_to request.referer
+    render :change_btn
   end
 end
